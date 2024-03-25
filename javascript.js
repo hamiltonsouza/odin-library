657bfb7b644cd1c93d1b560e171df6d579e07f84
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-03-25 20:23:42 -0300

    Add modal and table functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-const myLibrary = ['The Lightning Thief', 'The Sea Of Monsters', "The Titan's Curse", 'The Battle Of The Labyrinth', 'The Last Olympian'];
+const lightningThief = {title:'The Lightning Thief', author:'Rick Riordan',pages: 377,read: true};
+
+const myLibrary = [lightningThief];
+
+// , 'The Sea Of Monsters', "The Titan's Curse", 'The Battle Of The Labyrinth', 'The Last Olympian'
 
 function Book(title,author,pages,read){
     this.title = title;
@@ -16,8 +20,30 @@ function addBookToLibrary(book) {
 
 let bookList = document.querySelector(".book-list");
 
-myLibrary.forEach(book=>{
-    const currentElement = document.createElement("li");
-    currentElement.textContent = book;
-    bookList.appendChild(currentElement);
+myLibrary.forEach( book => {
+    const tableRow = document.createElement("tr");
+    Object.keys(lightningThief).forEach(key => {
+        const currentElement = document.createElement("td")
+        currentElement.textContent = book[key];
+        tableRow.appendChild(currentElement);
+    });
+    bookList.appendChild(tableRow);
 });
+
+var newBookModal = document.getElementById("new-book-modal");
+var newBookButton = document.getElementById("new-book-button");
+var closeButton = document.getElementsByClassName("close-button")[0];
+
+newBookButton.onclick = function() {
+    newBookModal.style.display = "block";
+}
+
+closeButton.onclick = function() {
+    newBookModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == newBookModal) {
+    newBookModal.style.display = "none";
+  }
+}
