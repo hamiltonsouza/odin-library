aa7196889ff218716847fa17e34ca75082fa1fdc
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-03-27 19:43:27 -0300

    Improve add book to library function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-var newBookModal = document.getElementById("new-book-modal");
-var newBookButton = document.getElementById("new-book-button");
-var closeButton = document.getElementsByClassName("close-button")[0];
+let newBookModal = document.getElementById("new-book-modal");
+let newBookButton = document.getElementById("new-book-button");
+let closeButton = document.getElementById("close-button");
 
 newBookButton.onclick = function() {
     newBookModal.style.display = "block";
@@ -16,37 +16,34 @@ window.onclick = function(event) {
   }
 };
 
-const lightningThief = {title:'The Lightning Thief', author:'Rick Riordan', pages: 377, read: true};
+const myLibrary = [];
 
-const myLibrary = [lightningThief];
-
-// , 'The Sea Of Monsters', "The Titan's Curse", 'The Battle Of The Labyrinth', 'The Last Olympian'
-
-function Book(title,author,pages,read){
+function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return (`${this.title} by ${this.author}, ${this.pages} pages, ${read}.` )
-    };
+    // this.info = function() {
+    //     return (`${this.title} by ${this.author}, ${this.pages} pages, ${read}.` )
+    // };
 };
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    myLibrary.forEach( book => {
+        const tableRow = document.createElement("tr");
+        Object.keys(book).forEach(key => {
+            console.log(typeof(key));
+            const currentElement = document.createElement("td");
+            currentElement.textContent = book[key];
+            tableRow.appendChild(currentElement);
+        });
+        bookTable.appendChild(tableRow);
+    });
 };
 
 let bookTable = document.querySelector(".book-table");
 
-myLibrary.forEach( book => {
-    const tableRow = document.createElement("tr");
-    Object.keys(lightningThief).forEach(key => {
-        const currentElement = document.createElement("td");
-        currentElement.textContent = book[key];
-        tableRow.appendChild(currentElement);
-    });
-    bookTable.appendChild(tableRow);
-});
 
 const submitButton = document.getElementById("submit-button");
 submitButton.addEventListener("click", submitButtonClick);
@@ -57,8 +54,6 @@ function submitButtonClick(event) {
     let bookPages = document.getElementById("book-pages").value;
     let bookRead = document.getElementById("book-read").value;
     const newBook =  new Book(bookTitle, bookAuthor, bookPages, bookRead);
-    console.log(bookTitle);
-    console.log(bookRead);
     addBookToLibrary(newBook);
-    console.log(newBook);
+    newBookModal.style.display = "none";
 };
